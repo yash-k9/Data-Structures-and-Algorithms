c7a6971ef71ef2ce3c34346fd3547feed6a4cc61
Kurella Yash <59912430+yash-k9@users.noreply.github.com>
2020-07-20 20:07:26 +0530

    Added Binary Search

---
+++ Searching and Sorting Algorithms/BinarySearch.java
@@ -0,0 +1,50 @@
+/*
+	Binary Search algorithm searches for the target in a sorted array 
+	by dividing the array into half. It returns if search key is found
+	at mid. If search key is not found and  mid element is less than 
+	the target, it narrows down the interval to upper half and if the 
+	mid element is larger then narrows down the interval to lower half.
+
+	Time Complexity : O(Log n)
+	Space Complexity : O(1)
+
+*/
+
+public class BinarySearch{
+
+	public static int binarySearch(int[] arr, int target){
+		int size = arr.length;
+		int low = 0;
+		int high = size - 1;
+		
+		while (low <= high) {
+			int mid = low + ((high - low) / 2);
+
+			if(arr[mid] == target){
+				return mid;
+			}
+			else if(arr[mid] < target){
+				low = mid + 1;
+			}
+			else if(arr[mid] > target){
+				high = mid - 1;
+			}
+		}
+		return low;
+	}
+
+	public static void main(String[] args){
+		int arr[] = {2, 3, 4, 7, 15, 20, 25, 45};
+		int size = arr.length;
+		int target = 5;
+
+		int result = binarySearch(arr, target);
+		if(result != -1){
+			System.out.println("The element is found at index " + result);
+		}
+		else{
+			System.out.println("The element is not found.");
+		}
+		
+	}
+}
