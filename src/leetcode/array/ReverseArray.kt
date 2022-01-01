8dbc3d6fe97e7107135db6abacdaefd9ad9da417
Kurella Yash <yashkurella.9@gmail.com>
2022-01-01 23:54:45 +0530

    Added Reverse Array

---
+++ src/leetcode/array/ReverseArray.kt
@@ -0,0 +1,15 @@
+package leetcode.array
+
+class ReverseArray {
+    fun reverse(nums: IntArray, start: Int, end: Int){
+        var start = start; var end = end
+
+        while(start < end){
+            val temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start += 1
+            end -= 1
+        }
+    }
+}
